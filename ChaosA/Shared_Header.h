133aa6299633ac45b23f35504c473eba538642d1
Yibo Liang <yibo.insane@gmail.com>
2017-03-19 23:41:01 +0000

    Working in comments

--- ChaosA/Shared_Header.h
+++ ChaosA/Shared_Header.h
@@ -1,1 +1,35 @@
 #pragma once
+
+/* 
+
+Cells {
+	states:
+		hunger
+		running speed
+		rushing // rushing skill, forwawrd with fast speed
+		direction // running on direction
+		perception{
+			36 * {
+				(0 - max vision) object distance
+				(0 / 1 / 2) object [neutral, same species, or different species]
+			}
+		}
+
+	property
+		vision
+		size
+		speed => max velocity 
+		agility => max turing direction angle
+		neural network {
+			layers{
+				[nodes]{
+					weights,
+					value
+				}
+			}
+		}
+
+}
+
+
+*/
